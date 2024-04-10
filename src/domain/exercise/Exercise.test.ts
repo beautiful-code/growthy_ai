8628c890c49d5a7b4579e34bd38d40f3c54f6fdf
pranavpb <pranavpb@sojern.com>
2024-04-10 16:37:25 -0500

    code cleanup - remove un-necessary code and files

--- src/domain/exercise/Exercise.test.ts
+++
@@ -1,9 +0,0 @@
-import { Exercise } from "./Exercise";
-
-describe("Exercise", () => {
-  it(" should have a name", () => {
-    const exercise = new Exercise("Exercise 1");
-
-    expect(exercise.name).toBe("Exercise 1");
-  });
-});
