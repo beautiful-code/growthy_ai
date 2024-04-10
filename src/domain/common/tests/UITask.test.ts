97ffe3a7275579cddbee055f21e1a5c86fb08975
pranavpb <pranavpb@sojern.com>
2024-04-10 17:30:46 -0500

    add uuid to uiTask constructor

--- src/domain/common/tests/UITask.test.ts
+++ src/domain/common/tests/UITask.test.ts
@@ -2,22 +2,32 @@ import { UITask } from "domain/common/UITask";
 
 describe("UITask", () => {
   it("should get constucted with a xml string", () => {
-    const task = new UITask("<Task name='task1' />");
+    const task = new UITask({
+      uuid: "1",
+      xml: "<Task name='task1' />",
+    });
+    console.log(task);
     expect(task).toBeDefined();
     expect(task.getText()).toBe("task1");
     expect(task.getChecked()).toBe(false);
   });
 
   it("should update text", () => {
-    const task = new UITask("<Task name='task1' />");
+    const task = new UITask({
+      uuid: "1",
+      xml: "<Task name='task1' />",
+    });
     task.updateText("task2");
     expect(task.getText()).toBe("task2");
-    expect(task.getUIStatelessXML()).toBe('<Task name="task2" />');
-    expect(task._xml).toBe('<Task name="task2"/>');
+    expect(task.getUIStatelessXML()).toBe('<Task name="task2" uuid="1" />');
+    expect(task._xml).toBe('<Task name="task2" uuid="1"/>');
   });
 
   it("should get constucted with a xml string with checked", () => {
-    const task = new UITask("<Task name='task1' checked='true' />");
+    const task = new UITask({
+      uuid: "1",
+      xml: "<Task name='task1' checked='true' />",
+    });
     expect(task).toBeDefined();
     expect(task.getText()).toBe("task1");
     expect(task.getChecked()).toBe(true);
@@ -25,10 +35,13 @@ describe("UITask", () => {
 
   describe("when checked is updated", () => {
     it("the xml string should have checked attribute set to true", () => {
-      const task = new UITask("<Task name='task1' />");
+      const task = new UITask({
+        uuid: "1",
+        xml: "<Task name='task1' />",
+      });
       task.updateChecked(true);
-      expect(task._xml).toBe('<Task name="task1" checked="true"/>');
-      expect(task.getUIStatelessXML()).toBe('<Task name="task1" />');
+      expect(task._xml).toBe('<Task name="task1" uuid="1" checked="true"/>');
+      expect(task.getUIStatelessXML()).toBe('<Task name="task1" uuid="1" />');
     });
   });
 });
