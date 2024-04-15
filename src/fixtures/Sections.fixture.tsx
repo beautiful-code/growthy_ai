c02e1eb7acd60638b3682e98bd569e2095fdd9f1
Ravi Bhim <ravi.bhim@yahoo.com>
2024-04-15 12:24:58 +0530

    Refactor WIP - changed Sections to SectionList

--- src/fixtures/Sections.fixture.tsx
+++ src/fixtures/Sections.fixture.tsx
@@ -1,38 +1,53 @@
-import { Sections } from "preview/components/Sections";
-import { FixtureWrapper } from "./FixtureWrapper";
-import { PublicationSection } from "types";
+import { Sections } from "preview/components/Sections"
+import { PublicationSection } from "types"
+import { FixtureWrapper } from "./FixtureWrapper"
 
 const publicationSections: PublicationSection[] = [
     {
-        "sectionTitle": "Introduction to My Sanskrit Journey",
-        "content": [
-            "Dummy content 5"
-        ]
+        sectionTitle: "Introduction to My Sanskrit Journey",
+        sectionContent: `Dummy content 5`
     },
     {
-        "sectionTitle": "Discovering The Sanskrit Channel",
-        "content": [
-            "Dummy content 4"
-        ]
+        sectionTitle: "Discovering The Sanskrit Channel",
+        sectionContent: `Dummy content 1
+Dummy content 2
+Dummy content 3
+Dummy content 4
+Dummy content 5
+Dummy content 6
+Dummy content 7`
     },
     {
-        "sectionTitle": "My Daily Sanskrit Learning Routine",
-        "content": [
-            "Dummy content 3"
-        ]
+        sectionTitle: "My Daily Sanskrit Learning Routine",
+        sectionContent: `Dummy content 1
+Dummy content 2
+Dummy content 3
+Dummy content 4
+Dummy content 5
+Dummy content 6
+Dummy content 7`
     },
     {
-        "sectionTitle": "Reflections and Impressions",
-        "content": [
-            "Dummy content 1\nDummy content 2"
-        ]
+        sectionTitle: "Reflections and Impressions",
+        sectionContent: `Dummy content 1
+Dummy content 2
+Dummy content 3
+Dummy content 4
+Dummy content 5
+Dummy content 6
+Dummy content 7`
     }
 ]
 
 export default {
-    "Basecase": (
+    "With default selected index": (
         <FixtureWrapper>
-            <Sections publicationSections={publicationSections} />
+            <Sections publicationSections={publicationSections} selectedSectionIndex={0}  />
+        </FixtureWrapper>
+    ),
+    "With specific selected index": (
+        <FixtureWrapper>
+            <Sections publicationSections={publicationSections} selectedSectionIndex={2}  />
         </FixtureWrapper>
     ),
 }
