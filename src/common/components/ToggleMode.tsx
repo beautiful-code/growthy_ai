d2d1684603a77f905274ae06fde64da0341b9178
Hemanth.Kumar <hemanthkakumanu1@gmail.com>
2024-05-08 20:40:28 +0530

    Toggle modes in execution view

---
+++ src/common/components/ToggleMode.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { TExecutionModes } from "types";
+
+type Props = {
+  mode: TExecutionModes;
+  setMode?: React.Dispatch<React.SetStateAction<TExecutionModes>>;
+};
+
+export const ToggleMode: React.FC<Props> = ({ mode, setMode }) => {
+  const [selectedMode, setSelectedMode] = useState(mode);
+
+  const inactiveBg = "gray.200";
+  const activeBg = "green.500";
+  const activeColor = "white";
+
+  const handleModeChange = (mode: Props["mode"]) => {
+    setSelectedMode(mode);
+    if (setMode) {
+      setMode(mode);
+    }
+  };
+
+  const modes: TExecutionModes[] = ["Outline", "Notes", "Publish"];
+
+  return (
+    <Flex
+      alignItems="center"
+      bg={inactiveBg}
+      borderRadius="full"
+      w="250px"
+      p="2px"
+      h={"40px"}
+      justify="space-between"
+    >
+      {modes.map((mode) => (
+        <Button
+          key={mode}
+          flex="1"
+          bg={selectedMode === mode ? activeBg : "transparent"}
+          color={selectedMode === mode ? activeColor : "inherit"}
+          borderRadius="full"
+          onClick={() => handleModeChange(mode)}
+          justifyContent="center"
+          h={"100%"}
+          fontSize={"sm"}
+          _hover={{ bg: selectedMode === mode ? activeBg : "gray.300" }}
+        >
+          <Text>{mode}</Text>
+        </Button>
+      ))}
+    </Flex>
+  );
+};
